d1dccc5fccddd917e34fbe6e36047449bd8be7b8
dada-yang <ydyinfofla@gmail.com>
2024-08-09 15:10:58 +0900

    !delete: 파일 삭제
    Types에서 User파일, server 파일 등 잘못 만든 파일 삭제

--- BE/src/types/Date.ts
+++
@@ -1,8 +0,0 @@
-// import { asNexusMethod, makeSchema } from "nexus";
-// import { GraphQLDate } from "graphql-iso-date";
-// // export const DateTime = GraphQLDate;
-// export const GQLDate = asNexusMethod(GraphQLDate, "date");
-
-// const schema = makeSchema({
-//   types: [GQLDate], // Add Scalar to Array
-// });
